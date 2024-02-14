e5fbaa6be21df9f38ae1e91a1cd8c59323a2eb00
piyushmittal50 <mittalpiyush87483@gmail.com>
2024-02-14 21:22:52 +0530

    NetflixGPT

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
-
+import Header from './components/Header';
+import Body from './components/Body';
 function App() {
   return (
-    <div className="text-3xl font-bold text-red-500 ">
-     Netflix chatGPT
+    <div>
+      <Header/>
+      <Body/>
     </div>
   );
 }
