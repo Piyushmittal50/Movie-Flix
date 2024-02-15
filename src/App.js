73351af468edaaf7f32a0fed5260c453897fe302
piyushmittal50 <mittalpiyush87483@gmail.com>
2024-02-15 18:01:52 +0530

    NetflixGPT

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import Header from './components/Header';
 import Body from './components/Body';
 function App() {
   return (
     <div>
-      <Header/>
       <Body/>
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Header from "./Header";
 // rafce
 const Login = () => {
+  const [isSignInForm,setSignInForm] = useState(true);
+  const ToggleSignIn = () => {
+    setSignInForm(!isSignInForm);
+  }
   return (
-    <div>Login</div>
-  )
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/c0b69670-89a3-48ca-877f-45ba7a60c16f/2642e08e-4202-490e-8e93-aff04881ee8a/IN-en-20240212-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="logo"
+        />
+      </div>
+      <form className="absolute w-3/12 bg-black my-36 mx-auto left-0 right-0 p-8 text-white rounded-lg bg-opacity-80">
+        <h1 className="text-3xl font-bold py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Enter Name"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+        )}
+
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <input
+          type="email"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <button className="p-4 my-6 w-full rounded-lg bg-red-700">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-2 cursor-pointer" onClick={ToggleSignIn}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already Registered? Sign In Now"}
+        </p>
+      </form>
+    </div>
+  );
 }
 
 export default Login
